864edd13eed8b0ede854526e47bd31169266b124
Taavi Kala <taavi.kala@wisemedia.ee>
2018-03-07 11:19:53 +0200

    Add working Vagrant file and bootstrap.sh

---
+++ Vagrantfile
@@ -0,0 +1,17 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+Vagrant.configure('2') do |config|
+  # FIXME: When upgrading to a future version of Ubuntu check if the workaround
+  # near the top of bootstrap.sh is still needed. If not, please delete it.
+  config.vm.box      = 'ubuntu/artful64' # 17.10
+  config.vm.hostname = 'techsisters'
+
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
+
+  config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
+
+  config.vm.provider 'virtualbox' do |v|
+    v.memory = ENV.fetch('RAILS_DEV_BOX_RAM', 2048).to_i
+    v.cpus   = ENV.fetch('RAILS_DEV_BOX_CPUS', 2).to_i
+  end
+end
---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# The output of all these installation steps is noisy. With this utility
+# the progress report is nice and concise.
+function install {
+    echo installing $1
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
+
+echo expanding root file system
+sudo resize2fs /dev/sda1
+
+echo adding swap file
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+
+echo updating package information
+apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
+apt-get -y update >/dev/null 2>&1
+
+install 'development tools' build-essential
+
+install Ruby ruby2.5 ruby2.5-dev
+update-alternatives --set ruby /usr/bin/ruby2.5 >/dev/null 2>&1
+update-alternatives --set gem /usr/bin/gem2.5 >/dev/null 2>&1
+
+echo installing Bundler
+gem install bundler -N >/dev/null 2>&1
+
+install 'Nokogiri dependencies' zlib1g-dev libffi-dev libxml2 libxml2-dev libxslt1-dev
+install 'ExecJS runtime' nodejs
+install Git git
+install SQLite sqlite3 libsqlite3-dev
+
+# Needed for docs generation.
+update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+
+echo 'All set!'
